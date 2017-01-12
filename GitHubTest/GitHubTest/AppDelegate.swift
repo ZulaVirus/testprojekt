22ac9cdc4fe625a4021c4518fbd1d7ad3a88180d
Bjarne Pedersen <bjarne@Bjarnes-MacBook-Pro.local>
2017-01-12 14:24:12 +0100

    first projekt start

---
+++ GitHubTest/GitHubTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitHubTest
+//
+//  Created by Bjarne Pedersen on 12/01/2017.
+//  Copyright © 2017 Bjarne Pedersen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitHubTest/GitHubTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Bjarne Pedersen on 12/01/2017.
+//  Copyright © 2017 Bjarne Pedersen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
